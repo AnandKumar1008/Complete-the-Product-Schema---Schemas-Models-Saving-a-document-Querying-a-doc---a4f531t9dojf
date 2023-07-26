12fb1fa42af12fe61dcb150c40b9d629dac67523
AnandKumar <anandkumar031199@gmail.com>
2023-07-26 16:06:03 +0530

    make it commit

--- models/product.js
+++ models/product.js
@@ -1,6 +1,24 @@
-
-var mongoose= require("mongoose");
+var mongoose = require("mongoose");
 
 //write your schema Here with name of schema as productSchema
+const productSchema = {
+  name: {
+    type: String,
+    required: true,
+  },
+  Quantity: {
+    type: Number,
+    required: true,
+  },
+  Price: {
+    type: Number,
+    required: true,
+  },
+  instock: {
+    type: String,
+    enum: ["Yes", "No"],
+    required: true,
+  },
+};
 
-module.exports =mongoose.model("products",productSchema);
+module.exports = mongoose.model("products", productSchema);
